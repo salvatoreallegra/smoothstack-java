19a70e337911eec859f256041ce20c970311a2cf
Salvatore Allegra <salallegra@gmail.com>
2020-10-14 12:33:41 -0400

    Complete assignment 1

--- guessnumber/src/Main.java
+++ guessnumber/src/Main.java
@@ -1,36 +1,54 @@
+import java.util.Random;
 import java.util.Scanner;
 
 /*****
  * 
- * @author salal
- * Guess numbers game
+ * @author salal Guess numbers game
  */
 public class Main {
 
 	static int numberOfGuesses = 0;
 	static int guessedNumber = 0;
+	static boolean guessedCorrectly;
+	static Random rand = new Random();
+	static int correctGuess = rand.nextInt(100);
 	static Scanner sc = new Scanner(System.in);
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		while(numberOfGuesses < 5) {
-			guessingGameEngine();
-			numberOfGuesses++;			
+		while (numberOfGuesses < 5) {
+			guessedCorrectly = guessingGameEngine();
+			numberOfGuesses++;
+			if (guessedCorrectly) {
+
+				break;
+
+			}
 		}
-		System.out.println("You are out of guesses, thanks for playing");
+		if (guessedCorrectly) {
+			System.out.println("You are within +- 10 of the correct number, congratulations!");
+		} else {
+			System.out.println("You are out of guesses, thanks for playing");
+			System.exit(0);
+		}
 
 	}
-	
-	public static void guessingGameEngine() {
-		System.out.println("Gueesed");
+
+	public static boolean guessingGameEngine() {
+
+		System.out.println(correctGuess);
 		guessedNumber = sc.nextInt();
-		while(guessedNumber < 1 || guessedNumber  > 100) {
+		while (guessedNumber < 1 || guessedNumber > 100) {
 			System.out.println("Number out of Range, please enter a number between 1 and 100");
 			guessedNumber = sc.nextInt();
-			
-			
+
 		}
-		
+		if (guessedNumber >= (correctGuess - 10) && guessedNumber <= (correctGuess + 10)) {
+			return true;
+		} else {
+			return false;
+		}
+
 	}
 
 }
