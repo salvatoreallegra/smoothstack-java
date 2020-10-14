777b367947d7d41a8682c0a9c95978f3d67a9dfd
Salvatore Allegra <salallegra@gmail.com>
2020-10-14 13:23:10 -0400

    Add player and pile classes

---
+++ game/src/game/Main.java
@@ -0,0 +1,11 @@
+package game;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean gameOver;
+		while(!gameOver)
+	}
+
+}
---
+++ game/src/model/Pile.java
@@ -0,0 +1,9 @@
+package model;
+
+public class Pile {
+	
+	private int chipStack;
+	
+	
+
+}
