8d47ed797d7c47a9c43e221832f1d838a33bc9a7
Salvatore Allegra <salallegra@gmail.com>
2020-10-17 12:31:07 -0400

    Add branch pojo

--- library-management/src/com/salallegra/library/Entity/Branch.java
+++ library-management/src/com/salallegra/library/Entity/Branch.java
@@ -1,5 +1,29 @@
 package com.salallegra.library.Entity;
 
 public class Branch {
+	
+	Integer branchID;
+	String branchName;
+	String branchAddress;
+	public Integer getBranchID() {
+		return branchID;
+	}
+	public void setBranchID(Integer branchID) {
+		this.branchID = branchID;
+	}
+	public String getBranchName() {
+		return branchName;
+	}
+	public void setBranchName(String branchName) {
+		this.branchName = branchName;
+	}
+	public String getBranchAddress() {
+		return branchAddress;
+	}
+	public void setBranchAddress(String branchAddress) {
+		this.branchAddress = branchAddress;
+	}
+	
+	
 
 }
--- library-management/src/com/salallegra/library/service/LibrarianService.java
+++ library-management/src/com/salallegra/library/service/LibrarianService.java
@@ -1,5 +1,46 @@
 package com.salallegra.library.service;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import com.salallegra.library.Entity.Author;
+import com.salallegra.library.Entity.Book;
+import com.salallegra.library.dao.AuthorDAO;
+import com.salallegra.library.dao.BookDAO;
+
 public class LibrarianService {
 
+	public ConnectionUtil conUtil = new ConnectionUtil();
+
+	public String addBook(Book book) throws SQLException {
+		Connection conn = null;
+		try {
+			conn = conUtil.getConnection();
+			BookDAO bdao = new BookDAO(conn);
+			AuthorDAO adao = new AuthorDAO(conn);
+			if (book.getTitle() != null && book.getTitle().length() > 45) {
+				return "Book Title cannot be empty and should be 45 char in length";
+			}
+			book.setBookId(bdao.addBookWithPk(book));
+			for (Author a : book.getAuthors()) {
+				adao.addBookAuthors(book.getBookId(), a.getAuthorId());
+			}
+			// Do the same for genres/branche etc.
+			// for(Author a: book.getAuthors()) {
+			// adao.addBookAuthors(book.getBookId(), a.getAuthorId());
+			// }
+			conn.commit();
+			return "Book added sucessfully";
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+			if (conn != null) {
+				conn.rollback();
+			}
+			return "Unable to add book - contact admin.";
+		} finally {
+			if (conn != null) {
+				conn.close();
+			}
+		}
+	}
 }
