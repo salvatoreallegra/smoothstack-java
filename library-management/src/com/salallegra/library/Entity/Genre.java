1a8cd50a3cdfc15372c0b87194bfae1fab220110
Salvatore Allegra <salallegra@gmail.com>
2020-10-20 12:22:27 -0400

    Create genre pojo and genra dao

--- library-management/src/com/salallegra/library/Entity/Genre.java
+++ library-management/src/com/salallegra/library/Entity/Genre.java
@@ -1,5 +1,29 @@
 package com.salallegra.library.Entity;
 
 public class Genre {
+	
+	Integer genreId;
+	String genreName;
+	public Genre(Integer genreId, String genreName) {
+		super();
+		this.genreId = genreId;
+		this.genreName = genreName;
+	}
+	public Integer getGenreId() {
+		return genreId;
+	}
+	public void setGenreId(Integer genreId) {
+		this.genreId = genreId;
+	}
+	public String getGenreName() {
+		return genreName;
+	}
+	public void setGenreName(String genreName) {
+		this.genreName = genreName;
+	}
+	
+	
+	
+	
 
 }
--- library-management/src/com/salallegra/library/dao/GenreDAO.java
+++ library-management/src/com/salallegra/library/dao/GenreDAO.java
@@ -3,10 +3,39 @@
  */
 package com.salallegra.library.dao;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.salallegra.library.Entity.Book;
+import com.salallegra.library.Entity.Genre;
+
 /**
  * @author ppradhan
  *
  */
-public class GenreDAO {
+public class GenreDAO extends BaseDAO<Genre>{
+	
+		
+	public GenreDAO(Connection conn) {
+		super(conn);
+	}
+	
+	@Override
+	public List<Genre> extractData(ResultSet rs) throws SQLException, ClassNotFoundException {
+		List<Genre> genres = new ArrayList<>();
+		AuthorDAO adao = new AuthorDAO(conn);
+		while (rs.next()) {
+			Genre b = new Genre(rs.getInt("genre_id"), rs.getString("genre_name"));
+			//b.setAuthors(adao.read("select * from tbl_author where authorId IN (select authorId from tbl_book_authors where bookId = ?)", new Object[] {b.getBookId()}));
+			//b.setGenres()
+			genres.add(b);
+		}
+		return genres;
+	}
+	
+	
 
 }
